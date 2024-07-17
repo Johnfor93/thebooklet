2ae636b75214f05d7d3909d49629cb470de05469
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2024-07-17 20:47:08 +0700

    set pinia, i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   postcss: {
     plugins: {
@@ -12,6 +11,24 @@ export default defineNuxtConfig({
   },
 
   plugins: ["~/plugins/preline.client.ts"],
-
-  compatibilityDate: '2024-07-03',
-})
+  compatibilityDate: "2024-07-03",
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  pinia: {
+    storesDirs: ["./stores/**"],
+  },
+  i18n: {
+    lazy: true,
+    langDir: "lang",
+    defaultLocale: "id",
+    locales: [
+      {
+        code: "en",
+        file: "en-US.json",
+      },
+      {
+        code: "id",
+        file: "id-ID.json",
+      },
+    ],
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-    './node_modules/preline/preline.js',
-  ],
+  content: ["./components/**/*.{js,vue,ts}", "./layouts/**/*.vue", "./pages/**/*.vue", "./plugins/**/*.{js,ts}", "./app.vue", "./error.vue", "./node_modules/preline/preline.js"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('preline/plugin'),
-  ],
-}
+  plugins: [require("preline/plugin")],
+};
