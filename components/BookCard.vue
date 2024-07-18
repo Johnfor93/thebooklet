81be3877c13f10d47ad79cdf80ca99cfbb2e4ca2
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2024-07-18 21:54:17 +0700

    list book

--- components/BookCard.vue
+++ components/BookCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <img class="rounded-t-lg aspect-[5/6]" src="https://bukukita.com/babacms/displaybuku/116969_f.jpg" alt="" />
+    <img class="rounded-t-lg aspect-[5/6]" :src="book.img" alt="" />
     <div class="p-5">
       <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white truncate">{{ book.title }}</h5>
       </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ book.description }}</p>
       <a
         href="#"
         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -18,3 +18,18 @@
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  book: {
+    type: {},
+    required: true,
+    default: {
+      title: "Noteworthy technology acquisitions 2021",
+      description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      img: "https://bukukita.com/babacms/displaybuku/116969_f.jpg",
+      id: "1",
+    },
+  },
+});
+</script>
