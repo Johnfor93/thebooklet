899c1ab4786d8a0124a18e025de624d8cb51e2eb
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2024-07-18 21:33:34 +0700

    list book

---
+++ layouts/member.vue
@@ -0,0 +1,10 @@
+<template>
+  <sidebar></sidebar>
+  <div class="p-8 sm:ml-64">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import sidebar from "~/components/sidebar.vue";
+</script>
---
+++ pages/member/book.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 gap-8 gap-y-4">
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+  </div>
+</template>
+
+<script setup>
+import BookCard from "~/components/BookCard.vue";
+definePageMeta({
+  layout: "member",
+});
+</script>
