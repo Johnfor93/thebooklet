271373c8bd620dc9bb75c84533f4470e65d4f3a4
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2024-07-18 22:12:14 +0700

    log borrow, book detail

--- pages/member/book/index.vue
+++ pages/member/book/index.vue
@@ -1,4 +1,6 @@
 <template>
+  <h3 class="font-bold text-lg">Book List</h3>
+  <p class="mb-4 text-gray-700">Browse million knowlege on The Booklet</p>
   <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 gap-8 gap-y-4">
     <div v-for="book in books">
       <BookCard :book="book"></BookCard>
