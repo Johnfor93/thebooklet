699690c5f2bd28010ea33ec73411892f28f9bdb1
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2024-07-18 20:33:13 +0700

    Login and Register

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <script setup>
 const { locale, setLocale } = useI18n();
-import { useCounterStore } from '@/stores/counter'
-import { useUserStore } from '@/stores/user'
-import { computed } from 'vue'
+const switchLocalePath = useSwitchLocalePath();
+import { useCounterStore } from "@/stores/counter";
+import { useUserStore } from "@/stores/user";
+import { computed } from "vue";
 
-const store = useCounterStore()
-const userStore = useUserStore()
+const store = useCounterStore();
+const userStore = useUserStore();
 
-const powerTwoCount = computed(() => store.count * store.count)
+const powerTwoCount = computed(() => store.count * store.count);
 </script>
 
 <template>
@@ -15,15 +16,30 @@ const powerTwoCount = computed(() => store.count * store.count)
     <p class="text-red-500">{{ $t("welcome") }}</p>
     <p>{{ store.count }}</p>
     <p>Username: {{ userStore.username }}</p>
-    <button @click="store.increment" type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none">
+    <button
+      @click="store.increment"
+      type="button"
+      class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none"
+    >
       +
     </button>
-    <button @click="userStore.login" type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none">
+    <button
+      @click="userStore.login"
+      type="button"
+      class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none"
+    >
       login
     </button>
-    <button @click="userStore.logout" type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none">
+    <button
+      @click="userStore.logout"
+      type="button"
+      class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none"
+    >
       logout
     </button>
+
+    <NuxtLink :to="switchLocalePath('id')"> id </NuxtLink>
+    <NuxtLink :to="switchLocalePath('en')"> en </NuxtLink>
 
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
