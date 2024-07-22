5fd5af4fcd39d72e82c9421e14ea2a2299bbeb61
Jonathan Alphabet Sutanto <jonathan.alphabet@jasuindoad.local>
2024-07-22 08:01:54 +0700

    change return logic, add book amount again

--- server/api/add-return.post.js
+++ server/api/add-return.post.js
@@ -4,8 +4,18 @@ export default defineEventHandler(async (request) => {
   try {
     const dataBody = await readBody(request);
 
+    const dataBook = await sql`SELECT * FROM books WHERE id=${dataBody.idBook}`;
+    if (dataBook.rowCount == 0) {
+      return {
+        success: false,
+        message: "Book is not found",
+      };
+    }
+
     const data = await sql`INSERT INTO log_return(id_log_borrow, id_admin, return_date, sanction) VALUES(${dataBody.idLogBorrow}, ${dataBody.idAdmin}, ${dataBody.returnDate}, ${dataBody.sanction})`;
     console.log(data);
+
+    const dataUpdate = await sql`UPDATE books SET amount = ${dataBook.rows[0].amount + 1} WHERE id=${dataBody.idBook}`;
     return {
       success: true,
       message: "Data has been inserted",
